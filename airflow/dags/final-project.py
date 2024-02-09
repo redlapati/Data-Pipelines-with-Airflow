36386465519e7f9635f1ae1700645633eaf83586
redlapati <43528827+redlapati@users.noreply.github.com>
2024-02-09 12:50:49 +1100

    Create final-project.py

---
+++ airflow/dags/final-project.py
@@ -0,0 +1,112 @@
+from datetime import datetime, timedelta
+import pendulum
+import os
+from airflow.decorators import dag
+from airflow.operators.dummy_operator import DummyOperator
+from final_project_operators.stage_redshift import StageToRedshiftOperator
+from final_project_operators.load_fact import LoadFactOperator
+from final_project_operators.load_dimension import LoadDimensionOperator
+from final_project_operators.data_quality import DataQualityOperator
+from udacity.common.final_project_sql_statements import SqlQueries
+
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': pendulum.datetime(2018, 11, 1, 0, 0, 0, 0),
+    'end_date': pendulum.datetime(2018, 11, 30, 0, 0, 0, 0),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+}
+
+@dag(
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval='@hourly'
+)
+def final_project():
+
+    start_operator = DummyOperator(task_id='Begin_execution')
+
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events',
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        table="staging_events",
+        s3_bucket="udacity-dp-project",
+        s3_key="log-data/{{ logical_date.strftime('%Y') }}/{{ logical_date.strftime('%m') }}/{{ ds }}-events.json",
+        json_path="s3://udacity-dp-project/log_json_path.json",
+        region="us-east-1"
+    )
+
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs',
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        table="staging_songs",
+        s3_bucket="udacity-dp-project",
+        s3_key="song-data",
+        json_path="auto",
+        region="us-east-1"
+    )
+
+    load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table',
+        redshift_conn_id="redshift",
+        table="songplays",
+        sql=SqlQueries.songplay_table_insert
+    )
+
+    load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table',
+        redshift_conn_id="redshift",
+        table="users",
+        sql=SqlQueries.user_table_insert,
+        insert_mode="delete-load"
+    )
+
+    load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table',
+        redshift_conn_id="redshift",
+        table="songs",
+        sql=SqlQueries.song_table_insert,
+        insert_mode="delete-load"
+    )
+
+    load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table',
+        redshift_conn_id="redshift",
+        table="artists",
+        sql=SqlQueries.artist_table_insert,
+        insert_mode="delete-load"
+    )
+
+    load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table',
+        redshift_conn_id="redshift",
+        table="time",
+        sql=SqlQueries.time_table_insert,
+        insert_mode="delete-load"
+    )
+
+    run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks',
+        redshift_conn_id="redshift",
+        query = 'SELECT COUNT(*) FROM {}',
+        tables = ['songplays', 'users', 'songs', 'artists', 'time']
+    )
+
+    end_operator = DummyOperator(task_id='End_execution')
+
+    start_operator >> stage_events_to_redshift >> load_songplays_table
+    start_operator >> stage_songs_to_redshift >> load_songplays_table
+    load_songplays_table >> load_user_dimension_table >> run_quality_checks
+    load_songplays_table >> load_song_dimension_table >> run_quality_checks
+    load_songplays_table >> load_artist_dimension_table >> run_quality_checks
+    load_songplays_table >> load_time_dimension_table >> run_quality_checks
+    run_quality_checks >> end_operator
+
+
+final_project_dag = final_project()
